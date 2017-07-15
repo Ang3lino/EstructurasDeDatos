89967f84ac0e8f7fbf99e2862ea0c16d5e506585
Angel Lopez Manriquez <sigma271@outlook.com>
2017-07-15 13:12:35 -0500

    Add files via upload
    Nueva version de queue

---
+++ TADS/queue.c
@@ -0,0 +1,73 @@
+
+#include "queue.h"
+
+/*	front -> ... -> final -> NULL 	*/
+
+Queue *newQueue() {
+	Queue *queue = (Queue *) calloc (sizeof (Queue), 1);
+	queue->back = NULL;
+	queue->front = NULL;
+	queue->size = 0;
+	return queue;
+}
+
+void 
+enqueue (Queue *c, element e) {
+	node *newNode = (node *) malloc (sizeof (node));
+	newNode->e = e;
+	newNode->next = NULL;
+	if (!c->front)
+		c->front = c->back = newNode;
+	else {
+		c->back->next = newNode;
+		c->back = newNode;
+	}
+	c->size++;
+}
+
+/*	Si la queue esta vacia, se retorna basura.	*/
+element
+dequeue (Queue *c) {
+	element e;
+	if (c->front){
+		e = c->front->e;
+		node *del = c->front;
+		c->front = c->front->next;
+		free (del);
+		if (c->front == NULL)
+			c->back = NULL;
+		c->size--;
+	} else
+		puts ("WARNING: Empty element returned because you've ordered to dequeue an empty queue..."); 
+	return e;
+}
+
+void
+clear (Queue *c) {
+	element e;
+	while (c->front)
+		e = dequeue (c);
+}
+
+bool empty(Queue *c){ return c->size == 0; }
+
+element front (Queue *c){ return c->front->e; }
+
+element back (Queue *c){ return c->back->e; }
+
+int size(Queue *c){ return c->size; }
+
+element Element(Queue *c, int n) {
+	int pos = 1;
+	node * aux = c->front;
+	element e;
+	if (n <= size(c)) {
+		while (pos < n) {
+			aux = aux->next;
+			pos++;
+		}
+		e = aux->e;
+	}
+	return e;
+}
+
---
+++ TADS/queue.h
@@ -0,0 +1,80 @@
+﻿
+/*` Biblioteca del TAD queue dinamica 								     IPN-ESCOM
+ *  =================================================================================
+ *  
+ *  Esta es una implementacion dinamica eficiente del TAD queue, en el lenguaje C. 
+ *  
+ *  Para su uso simplemente agrega la cabecera #include "queue.h" al codigo fuente
+ *  y compilalo mediante el comando gcc fuente.c queue.c 
+ *  
+ *  para declarar una cola se usa la instruccion Queue *nombre = newQueue();
+ *  
+ *  Caracteristicas
+ *  --------------------------------------------------------------------------------
+ *  Al incluir la cabecera "queue.h", tambien se incluiran las bibliotecas stdbool.h
+ *  (por lo que se incluye el tipo de dato bool y sus valores true y false), stdio.h 
+ *  y stdlib.h. 
+ *  
+ *  Para agregar un tipo de dato se hace uso de una estructura element, en la cual 
+ *  se definiran los datos deseados en sus campos. Es obligatorio el uso de esta 
+ *  estructura para modificar y consultar el TAD.
+ *  
+ *  Notas
+ *  -------------------------------------------------------------------------------
+ *  Pueden fallar las funciones del tipo bool si no se tiene una version C99 o supe-
+ *  rior, actualizate, el futuro es hoy v:.
+ *  
+ *  Autor: Makigas Octubre 2015, primera version.
+ *  Modificada por: Lopez Manriquez Angel y Ontiveros Salazar Alan Enrique, Julio 2017.
+ *  Version: 3	
+ *  
+ *  Cualquier optimizacion al codigo, por favor, hazla saber a github/Ang3lino        */ 
+
+#ifndef QUEUE_H 
+#define QUEUE_H 
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*	Variables de la estructura element, puedes modificarla. */
+typedef struct element {
+	char Char,
+		 String[100];
+	int Integer;
+	double Double;
+	long Long;
+	bool Boolean; 
+} element;
+
+/*	Definir un nodo que será utilizado para almacenar una posición de la Queue (Nodo), 
+ *	lo que incluira a un element y a un apuntador al siguiente nodo.	*/
+typedef	struct node {
+	element e;
+	struct node *next;
+}node;
+
+typedef struct Queue {
+	node *front,
+		 *back;
+	int size;
+} Queue;
+
+/* Declaracion de queue. */
+Queue *newQueue();	
+
+/* Modificadores */
+void enqueue (Queue *c, element e);	
+element dequeue (Queue *c);	/* Retorna un elemento y lo elimina a la vez. */	
+void clear  (Queue *c);  /* Limpia la queue, se puede utilizar nuevamente. */
+
+/* Operaciones de consulta */
+int size (Queue *c);		
+bool empty (Queue *c);			
+element front (Queue *c);		
+element back (Queue *c);		
+element Element (Queue *c, int n); /* Elemento (Element): Recibe una Queue y una posicion, y devuelve 
+									   el n-esimo termino en la Queue. */
+
+#endif
+
