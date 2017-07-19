9db27a0a061a7ce296bd80d916c156a36d8a9416
Om3Ga-Z <sigma271@outlookc.com>
2017-07-19 14:11:42 -0500

    .

--- TADS/bst.c
+++ TADS/bst.c
@@ -1,24 +1,59 @@
 
 #include "bst.h"
 
-Bst *newBst(void) {
+Bst *
+newBst(void) {
 	Bst *tree = 0;
 	return tree;
 }
 
-void 
-insert (Bst **tree, const double data) {
-	if (*tree == NULL) {
-		*tree = calloc (sizeof (Bst), 1);
-		(*tree)->comparator = data;
-		(*tree)->left = NULL;
-		(*tree)->right = NULL;
-	} else if ((*tree)->comparator >= data)
-		insert (&(*tree)->left, data);
-	else 
-		insert (&(*tree)->right, data);
+Bst *
+bstCreateNode (const double data) {
+	Bst *tree = (Bst *) calloc (sizeof (Bst), 1);
+	tree->left = NULL;
+	tree->right = NULL;
+	tree->parent = NULL;
+	tree->comparator = data;
+	return tree;
 }
 
+static void 
+insertBstHelper (Bst **tree, Bst *parent, const double data) {
+	if (*tree == NULL) {
+		*tree = bstCreateNode (data);
+		(*tree)->parent = parent;
+	} else if ((*tree)->comparator >= data)
+		insertBstHelper (&(*tree)->left, *tree, data);
+	else 
+		insertBstHelper (&(*tree)->right, *tree, data);
+}
+
+void 
+bstInsert (Bst **tree, const double data) {
+	insertBstHelper (&(*tree), NULL, data);
+}
+
+void 
+insert (Bst **tree, const double data) {
+	Bst *before = NULL;
+	while (*tree) {
+		before = *tree;
+		if (data <= (*tree)->comparator) 
+			*tree = (*tree)->left;
+		else 
+			*tree = (*tree)->right;
+	}
+	*tree = bstCreateNode (data);
+	(*tree)->parent = before;
+}
+
+void 
+bstTransplant (Bst *tree, Bst *u, Bst *v) {
+	if (u->parent == NULL)
+		tree = u;
+
+}
+	
 bool 
 isLeaf (const Bst *tree) {
 	if (tree->left == NULL && tree->right == NULL)
@@ -40,22 +75,26 @@ bstExists (Bst *tree, const double data) {
 }
 
 void 
-inorderBst (Bst *tree) {
-	if (tree) {
-		inorderBst (tree->left);
-		printf ("%f ", tree->comparator);
-		inorderBst (tree->right);
+bstInorder (Bst *tree) {
+	if (!tree)
+		printf ("-");
+	else if (tree) {
+		printf("(");
+		bstInorder (tree->left);
+		printf ("%.2f ", tree->comparator);
+		bstInorder (tree->right);
+		printf(")");
 	}
 }
 
-Bst *searchBst (Bst *bst, const double data) {
+Bst *bstSearch (Bst *bst, const double data) {
 	if (bst) {
 		if (bst->comparator == data) 
 			return bst;
 		if (data <= bst->comparator)
-			return searchBst (bst->left, data);
+			return bstSearch (bst->left, data);
 		else
-			return searchBst (bst->right, data);
+			return bstSearch (bst->right, data);
 	} 
 	return newBst();
 }
@@ -73,12 +112,12 @@ Bst *bstMax (Bst *bst) {
 }
 
 Bst *bstSuccessor (Bst *bst, const double data) { 
-	Bst *x = searchBst (bst, data);
+	Bst *x = bstSearch (bst, data);
 	return bstMin (x->right);
 }
 
 Bst *bstPredecessor (Bst *bst, const double data) {
-	Bst *x = searchBst (bst, data);
+	Bst *x = bstSearch (bst, data);
 	return bstMax (x->left);
 }
 
--- TADS/bst.h
+++ TADS/bst.h
@@ -2,27 +2,31 @@
 #ifndef BST_H 
 #define BST_H
 
+#define NIL 0
+
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
 
 typedef struct element {
 	/* what you want */
-
+	double comparator;
 } element;
 
 typedef struct bst {
 	element e;
 	double comparator;
-	struct bst *left;
-	struct bst *right;
+	struct bst *left,
+			   *right,
+			   *parent;
 } Bst;
 
-Bst *newBst (void);
+void bstInsert (Bst **, const double);
 void insert (Bst **, const double);
+void bstInorder (Bst *);
 bool isLeaf (const Bst *);
 bool bstExists (Bst *, const double);
-void inorderBst (Bst *);
+Bst *newBst (void);
 Bst *bstSearch (Bst *, const double);
 Bst *bstMin (Bst *);
 Bst *bstMax (Bst *);
